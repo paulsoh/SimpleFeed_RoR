63d694cfb49ef37a2d71f241520f59933e53d167
paulsoh <yongdoree@gmail.com>
2016-05-20 10:30:07 +0900

    init: SimpledFeed Rails 3.2.0 project

---
+++ SimpleFeed/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleFeed::Application.config.secret_token = '768d75d5b6b33647139570d50ab9cb7772f42afe6e361d06ae53d3fe2d0d7132e2f4e766470ef8688cdbd234253f893ec031b338eb747679d19addc9385fc29b'
