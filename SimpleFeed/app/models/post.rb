bb8da335603946f08433b7dd59b2c002010b160d
paulsoh <yongdoree@gmail.com>
2016-05-30 13:39:42 +0900

    Add post title validation method

--- SimpleFeed/app/models/post.rb
+++ SimpleFeed/app/models/post.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # The Model: Post
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :url, :title, :tags_attributes
@@ -12,4 +13,14 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :tags, 
                                 allow_destroy: :true, 
                                 reject_if: proc { |attrs| attrs.all? {|_k, v| v.blank? } }
+
+  validate :validate_post_title
+
+  def validate_post_title
+    invalid_words = %w(광고 도박 무료 혜택 충전 성인) 
+    words = []
+    invalid_words.each { |word| words << word unless title.match(word).blank? }
+    errors.add(:title, 
+               "Invalid word in title: #{words.join(', ')}") unless words.blank?
+  end
 end
