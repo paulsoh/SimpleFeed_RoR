6b717335d1c7ca60e801f720212ba412a5fc480c
paulsoh <yongdoree@gmail.com>
2016-05-31 10:57:47 +0900

    Add validation for non duplicated post

--- SimpleFeed/app/models/post.rb
+++ SimpleFeed/app/models/post.rb
@@ -15,6 +15,9 @@ class Post < ActiveRecord::Base
                                 reject_if: proc { |attrs| attrs.all? {|_k, v| v.blank? } }
 
   validate :validate_post_title
+  validate :validate_non_duplicated_post
+
+  private
 
   def validate_post_title
     invalid_words = %w(광고 도박 무료 혜택 충전 성인) 
@@ -22,5 +25,23 @@ class Post < ActiveRecord::Base
     invalid_words.each { |word| words << word unless title.match(word).blank? }
     errors.add(:title, 
                "Invalid word in title: #{words.join(', ')}") unless words.blank?
+  end
+
+  def validate_non_duplicated_post
+    if title == last_post_title
+      errors.add(:title, 
+                 'Identical title to last post')
+    elsif name == last_post_name 
+      errors.add(:name, 
+                 'Identical name to last post')
+    end
+  end
+
+  def last_post_name
+    Post.last.name
+  end
+
+  def last_post_title
+    Post.last.title
   end
 end
