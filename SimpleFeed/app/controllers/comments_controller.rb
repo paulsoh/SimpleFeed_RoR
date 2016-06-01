4b9d87370318e4569bde0aa48a32da6197818e21
paulsoh <yongdoree@gmail.com>
2016-06-01 20:10:37 +0900

    Add before_filter, validation for comments * Add find_post, find_comment to before_filter * Add validation for body length and duplication

--- SimpleFeed/app/controllers/comments_controller.rb
+++ SimpleFeed/app/controllers/comments_controller.rb
@@ -1,14 +1,20 @@
 class CommentsController < ApplicationController
+  before_filter -> { find_post(:id, :post_id) }, only: [:create, :destroy]
+  before_filter :find_comment, only: [:destroy] 
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
     redirect_to post_path(@post)
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
+  end
+
+  private
+
+  def find_comment
+    @comment = @post.comments.find(params[:id])
   end
 end
--- SimpleFeed/app/models/comment.rb
+++ SimpleFeed/app/models/comment.rb
@@ -1,4 +1,26 @@
 class Comment < ActiveRecord::Base
   attr_accessible :body, :commenter, :post
   belongs_to :post
+
+  validates :commenter, presence: true
+  validates_length_of :body, presence: true, in: 5..140
+
+  validate :validate_non_duplicated_comment, on: :create
+
+  private
+
+  def validate_non_duplicated_comment
+    errors.add(:commenter, 'identical to last commenter') if commenter == last_commenter 
+    errors.add(:body, 'identical to last body') if body == last_body 
+  end
+
+  def last_commenter
+    return if post.comments.blank?
+    post.comments.last.commenter
+  end
+
+  def last_body
+    return if post.comments.blank?
+    post.comments.last.body
+  end
 end
