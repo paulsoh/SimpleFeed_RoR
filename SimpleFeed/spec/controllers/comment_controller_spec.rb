8f1164e79cc864df96aa5ad5056441de240b107c
paulsoh <yongdoree@gmail.com>
2016-06-02 01:25:37 +0900

    Add tests for #create in comment controller * made comment traits for validation testing

--- SimpleFeed/spec/controllers/comment_controller_spec.rb
+++ SimpleFeed/spec/controllers/comment_controller_spec.rb
@@ -9,39 +9,101 @@ describe CommentsController do
   #
   # =============================================================== 
 
-  shared_examples 'redirects to post#show' do
-    it 'redirects to post show view' do
-      subject
-      expect(response).to redirect_to post
+  shared_examples 'fails to create with invalid fields' do
+    it 'does not create post with invalid field' do
+      expect{ subject }.to change(Comment, :count).by(0)
     end
   end
+  
+  # ===============================================================
+  #
+  #                         SHARED EXAMPLES
+  #
+  # =============================================================== 
 
   describe '#create' do
+    let(:test_post) {create :post}
     context 'with invalid fields' do
-      context 'validation fails' do
-        it 'commenter is not present' do
-        end
-        it 'body is not present' do
-        end
-        it 'body is too short' do
-        end
-        it 'body is too long' do
-        end
-        it 'same commenter writes two in a row' do
-        end
-        it 'same body is written twice in a row' do
-        end
+      subject do 
+        post :create, 
+             post_id: test_post.id, 
+             comment: attributes_for(:invalid_comment)
       end
-      it 'post.comments does not change' do
-      end
+
       it 'redirect to post show view' do
+        subject
+        expect(response).to redirect_to test_post
+      end
+      include_examples 'renders 302 http status code'
+
+      context 'when commenter is not present' do
+        subject do 
+          post :create, 
+               post_id: test_post.id, 
+               comment: attributes_for(:comment, :no_commenter)
+        end
+        include_examples 'fails to create with invalid fields'
+      end
+      context 'when body is not present' do
+        subject do 
+          post :create, 
+               post_id: test_post.id, 
+               comment: attributes_for(:comment, :no_body)
+        end
+        include_examples 'fails to create with invalid fields'
+      end
+      context 'when body is not too short' do
+        subject do 
+          post :create, 
+               post_id: test_post.id, 
+               comment: attributes_for(:comment, :short_body)
+        end
+        include_examples 'fails to create with invalid fields'
+      end
+      context 'when body is not too long' do
+        subject do 
+          post :create, 
+               post_id: test_post.id, 
+               comment: attributes_for(:comment, :long_body)
+        end
+        include_examples 'fails to create with invalid fields'
+      end
+      context 'when comment is identical to previous comment' do
+        let!(:comment) {create(:comment, post: test_post)}
+        subject do 
+          post :create, 
+               post_id: test_post.id, 
+               comment: { commenter: comment.commenter,
+                          body: comment.body }
+        end
+        it 'identical commenter twice raises error' do
+          subject
+          expect(assigns['comment'].errors.full_messages)
+          .to include('Commenter identical to last commenter')
+        end
+        it 'identical body twice raises error' do
+          subject
+          expect(assigns['comment'].errors.full_messages)
+          .to include('Body identical to last body')
+        end
+        include_examples 'fails to create with invalid fields'
       end
     end
-    context 'valid field' do
+    context 'with valid fields' do
+      subject do 
+        post :create, 
+             post_id: test_post.id, 
+             comment: attributes_for(:comment)
+      end
       it 'comment is stored in post.comments' do
+        expect { subject }
+        .to change(Comment, :count).by(1)
       end
       it 'redirect to post show view' do
+        post :create, post_id: test_post.id
+        expect(response).to redirect_to test_post
       end
+      include_examples 'renders 302 http status code'
     end
   end
 
@@ -52,7 +114,10 @@ describe CommentsController do
     context 'when post id is invalid' do
       subject { delete :destroy, post_id: post.id, id: -1 } 
 
-      include_examples 'redirects to post#show'
+      it 'redirects to post show view' do
+        subject
+        expect(response).to redirect_to post
+      end
       include_examples 'renders 302 http status code'
     end
 
@@ -64,7 +129,10 @@ describe CommentsController do
         .to change(Comment, :count).by(-1)
       end
 
-      include_examples 'redirects to post#show'
+      it 'redirects to post show view' do
+        subject
+        expect(response).to redirect_to post
+      end
       include_examples 'renders 302 http status code'
     end
   end
--- SimpleFeed/spec/factories/comments.rb
+++ SimpleFeed/spec/factories/comments.rb
@@ -6,4 +6,25 @@ FactoryGirl.define do
     sequence(:commenter) { |n| "Commenter#{n}" } 
     sequence(:body) { |n| "New body#{n}" } 
   end
+
+  factory :invalid_comment, parent: :comment do
+    commenter nil
+    body nil
+  end
+
+  trait :no_commenter do 
+    commenter nil
+  end
+
+  trait :no_body do 
+    body nil
+  end
+
+  trait :long_body do 
+    body 'abc'*100
+  end
+
+  trait :short_body do 
+    body 'abc'
+  end
 end
